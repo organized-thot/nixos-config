4b23b2fb1763143f77bff6a51d32afbe11a480d5
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-05-24 03:16:03 -0500

    Added garuda nixos module

--- flake.nix
+++ flake.nix
@@ -5,12 +5,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    garuda.url = "gitlab:garuda-linux/garuda-nix-subsystem/stable";
 
     # Garuda module (assuming this is what you mean)
     garuda.url = "github:garuda-linux/garuda-nixos-config";
     garuda.inputs.nixpkgs.follows = "nixpkgs";
   };
 
+  
   outputs = { self, nixpkgs, home-manager, garuda, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
