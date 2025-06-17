102c146d9a8f8320272baf90ff26e8707c7ebd36
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-06-17 04:07:35 -0500

    Cleaned up declaration of NixOS modules and flakes setup. - Repaired `nixosConfigurations` - Moved Home Manager configuration to `home-manager.nix` - Corrected Garuda and Chaotic Nyx module declarations

--- flake.nix
+++ flake.nix
@@ -2,51 +2,36 @@
   description = "NixOS with Garuda, Home Manager, and Chaotic AUR";
 
   inputs = {
-    # Use NixOS unstable
+    # NixOS unstable
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # Home Manager for user environment
+    # home-manager module
     home-manager.url = "github:nix-community/home-manager";
-
     # Garuda Linux NixOS module
     garuda.url = "github:garuda-linux/garuda-nix-subsystem/stable";
-
     # Chaotic Nyx module
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, home-manager, garuda, chaotic, ... }:
-  let
-    system = "x86_64-linux";
-  in {
+  outputs = { self, nixpkgs, home-manager, garuda, chaotic }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
+        system = "x86_64-linux";
         modules = [
+          # Import hardware-configuration.nix
           ./hardware-configuration.nix
-          ./configuration.nix
-          chaotic.nixosModules.chaotic-nyx
-        ];
-      };
 
-      # Optionally define a Garuda configuration
-      garuda = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          garuda.nixosModules.default
+          # Import configuration.nix
           ./configuration.nix
-        ];
-      };
-    };
 
-    # Define Home Manager configuration
-    homeConfigurations = {
-      nix = home-manager.lib.homeManagerConfiguration {
-        inherit system;
-        username = "nix";
-        homeDirectory = "/home/nix";
-        configuration = import ./home.nix;
-        pkgs = import nixpkgs { inherit system; };
+          # Enable home-manager
+          ./home-manager.nix
+
+          # Enable Garuda Nix Subsystem
+          garuda.nixosModules.garudaLinux
+
+          # Enable Chaotic Nyx repository
+          chaotic.nixosModules.default
+        ];
       };
     };
   };
