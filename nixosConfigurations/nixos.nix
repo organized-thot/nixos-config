c7081bb03692b5618482bb7c9bcd80cf29015edd
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-06-28 01:40:05 -0500

    Modify nixosConfigurations/nixos.nix

--- nixosConfigurations/nixos.nix
+++ nixosConfigurations/nixos.nix
@@ -1,41 +1,38 @@
-{ inputs, ... }@flakeContext:
-let
-  nixosModule = { config, lib, pkgs, ... }: {
-    config = {
-      modules = [
-        # Import hardware-configuration.nix
-        ./hardware-configuration.nix
+{ config, lib, pkgs, ... }:
 
-        # Import configuration.nix
-        ./configuration.nix
+{
+  imports = [
+    # Import hardware-configuration.nix
+    ./hardware-configuration.nix
 
-        # Enable home-manager
-        ./home-manager.nix
+    # Import configuration.nix
+    ./configuration.nix
 
-        # Enable Garuda Nix Subsystem
-        garuda.nixosModules.garudaLinux
-
-        # Enable Chaotic Nyx repository
-        chaotic.nixosModules.default
+    # Enable home-manager module
+    ({ config, pkgs, ... }: {
+      imports = [
+        # This brings home-manager options into the NixOS configuration
+        # which is necessary for setting up users and enabling the service.
+        inputs.home-manager.nixosModules.home-manager
       ];
-      users.users.nix = {
-        isNormalUser = true;
-        password = "lazulinux";
+      home-manager.users.nix = {
+        home.stateVersion = "25.05"; # Or your desired home-manager state version
+        # You can define home-manager options directly here or import a home.nix
+        # For simplicity, if you have a separate home.nix for user configurations, you'd import it like:
+        # imports = [ ./home.nix ];
       };
-    };
+    })
+
+    # Enable Garuda Nix Subsystem
+    inputs.garuda.nixosModules.garudaLinux
+
+    # Enable Chaotic Nyx repository
+    inputs.chaotic.nixosModules.default
+  ];
+
+  # Define user account details here if not already in configuration.nix
+  users.users.nix = {
+    isNormalUser = true;
+    password = "lazulinux"; # Consider using encrypted password or setting it post-install
   };
-in
-	inputs.nixpkgs.lib.nixosSystem {
-	  modules = [
-	    nixosModule
-	  ];
-	  system = "x86_64-linux";
-	};
-	inputs.garuda.lib.garudaSystem {
-	  system = "x86_64-linux";
-	  modules = [
-	    configuration.nix
-	  ];
-	};
-      };
 }
