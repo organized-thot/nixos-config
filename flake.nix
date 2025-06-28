aceeaf736b5c872e79ec52f20117f5d249985d89
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-06-28 01:42:46 -0500

    Modify flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,22 @@
 {
   description = "NixOS configuration with Home Manager, Chaotic Nyx, and Garuda Linux NixOS submodule enabled.";
   inputs = {
-    nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable"; # Use nixpkgs-unstable for latest
     home-manager.url = "github:nix-community/home-manager";
     garuda.url = "gitlab:garuda-linux/garuda-nix-subsystem/stable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
-  outputs = inputs:
-    let
-      flakeContext = {
-        inherit inputs;
-      };
-    in
+  outputs = inputs@{ self, nixpkgs, home-manager, garuda, chaotic, ... }:
     {
       nixosConfigurations = {
-        nixos = import ./nixosConfigurations/nixos.nix flakeContext;
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            # Pass all inputs to the module
+            ({ ... }: { inherit inputs; })
+            ./nixos.md # This now imports your main NixOS configuration module
+          ];
+        };
       };
     };
 }
