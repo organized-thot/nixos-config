ed0782c2ae0fe0a89750a78573b0d1dafc8e2e0d
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-06-29 11:51:38 -0500

    Correct home-manager input in nixos.nix

--- nixos.nix
+++ nixos.nix
@@ -2,7 +2,7 @@
 imports = [
   ./hardware-configuration.nix
   ./configuration.nix
-  ./home-manager.md # Import the home-manager module
+  ./home-manager.nix # Import the home-manager module
 
   inputs.garuda.nixosModules.garudaLinux
   inputs.chaotic.nixosModules.default
