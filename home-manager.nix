535e9fee99ea887765fdff93d91dfdcaaeb6f4a0
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-06-29 11:47:14 -0500

    Modify home-manager.nix

--- home-manager.nix
+++ home-manager.nix
@@ -1,18 +1,20 @@
-# Enable home-manager
-home-manager.nixosModules.home-manager {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.users.nix = {
-        home = {
-            homeDirectory = "/home/nix";
-            username = "nix";
-            configuration = home-manager.lib.homeManagerConfiguration {
-                system = "x86_64-linux";
-                username = "nix";
-                homeDirectory = "/home/nix";
-                configuration = import ./home.nix;
-                pkgs = nixpkgs;
-            };
-        };
-    };
+# This file is meant to be imported as a NixOS module
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
+  home-manager.users.nix = {
+    home.homeDirectory = "/home/nix";
+    home.username = "nix";
+    home.stateVersion = "25.05"; # Or match your system.stateVersion
+
+    # If you have a separate home.nix for user configurations
+    imports = [ ./home.nix ]; # Assuming home.nix is in the same directory as this file
+  };
 }
