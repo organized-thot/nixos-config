0bcf49624690ff308f01127ddcc063ed3dd36ad7
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-06-29 22:11:24 -0500

    Modify home-manager.nix

--- home-manager.nix
+++ home-manager.nix
@@ -1,20 +1,14 @@
 # This file is meant to be imported as a NixOS module
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ... }:
 
 {
-  imports = [
-    inputs.home-manager.nixosModules.home-manager
-  ];
-
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-
-  home-manager.users.nix = {
-    home.homeDirectory = "/home/nix";
-    home.username = "nix";
-    home.stateVersion = "25.05"; # Or match your system.stateVersion
-
-    # If you have a separate home.nix for user configurations
-    imports = [ ./home.nix ]; # Assuming home.nix is in the same directory as this file
+  home-manager.users.nix = { pkgs, ... }: {
+    home.stateVersion = "25.05";
+    programs.zsh.enable = true;
+    programs.git = {
+      enable = true;
+      userName = "Organized Thot";
+      userEmail = "189394231+organized-thot@users.noreply.github.com";
+    };
   };
 }
