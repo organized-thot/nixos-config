c25e87e7007fbce19f4723b60c2075de1113e990
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-06-29 22:45:40 -0500

    Remove home-manager.users declaration from home-manager.nix

--- home-manager.nix
+++ home-manager.nix
@@ -1,21 +1,16 @@
 # This file is meant to be imported as a NixOS module
-{ config, pkgs, ... }:
-
-{
-  home-manager.users.nix = { pkgs, ... }: {
-    imports = config.garuda.home-manager.modules or [];
+{ config, pkgs, ... }: {
 
 # Custom Home Manager settings
 
-    programs.zsh.enable = true;
-    programs.git = {
-      enable = true;
-      userName = "Organized Thot";
-      userEmail = "189394231+organized-thot@users.noreply.github.com";
-    };
+  programs.zsh.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Organized Thot";
+    userEmail = "189394231+organized-thot@users.noreply.github.com";
+  };
 
     # Additional custom Home Manager modules and settings
 
-    home.stateVersion = "25.05";
-  };
+  home.stateVersion = "25.05";
 }
