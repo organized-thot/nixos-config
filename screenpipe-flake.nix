e03a5f119d4f101b0eed46e9f5858ba0446c9608
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-07-04 12:18:42 -0500

    Modify screenpipe-flake setup

--- screenpipe-flake.nix
+++ screenpipe-flake.nix
@@ -3,7 +3,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   outputs = { self, nixpkgs }:
     let
-      overlays = [ (import ../overlays/ffmpeg-overlay.nix) ];
+      overlays = [ (import ./overlays/ffmpeg-overlay.nix) ];
       pkgs = import nixpkgs { system = "x86_64-linux"; overlays = overlays; };
     in {
       packages.x86_64-linux.screen-pipe = pkgs.screen-pipe;
