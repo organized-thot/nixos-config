ca05a7ced4b99d4681eeec677ddeb9a92789a08e
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-07-21 06:03:22 -0500

    Replace flake.nix with garuda-nix-subsystem flake.nix

--- flake.nix
+++ flake.nix
@@ -1,39 +1,139 @@
 {
-  description = "NixOS configuration with Home Manager, Chaotic Nyx, and Garuda Linux NixOS submodule enabled.";
+  description = "Garuda Linux NixOS flake ❄️";
+
+  nixConfig.extra-substituters = [
+    "https://nyx.chaotic.cx"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+    "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+  ];
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    garuda.url = "github:garuda-linux/garuda-nix-subsystem/stable";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    chaotic.inputs.nixpkgs.follows = "nixpkgs";
-    screenpipe-flake.url = "path:./screenpipe-flake";
-  };
+    #
+    # OS internals
+    #
 
-  outputs = inputs@{ self, nixpkgs, home-manager, garuda, chaotic, ... }: {
-    overlays = {
-      default = import ./overlays/default.nix;
-      ffmpeg-overlay = import ./overlays/ffmpeg-overlay.nix;
+    # Chaotic's Nyx
+    chaotic-nyx = {
+      url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+      inputs.home-manager.follows = "home-manager";
     };
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hardware-configuration.nix
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        garuda.nixosModules.default
-        chaotic.nixosModules.default
-      ];
-      specialArgs = {
-        inherit inputs;
-        garuda = inputs.garuda;
-        chaotic = inputs.chaotic;
-        overlays = [
-          inputs.self.overlays.default
-          inputs.self.overlays.ffmpeg-overlay
-        ];
-      };
+
+    # If you need to, override this to use a different nixpkgs version
+    # by default we follow Chaotic Nyx' nyxpkgs-unstable branch
+    nixpkgs.follows = "chaotic-nyx/nixpkgs";
+
+    # Modules support for flakes
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "chaotic-nyx/nixpkgs";
+    };
+
+    # Have a local index of nixpkgs for fast launching of apps
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "chaotic-nyx/nixpkgs";
+    };
+
+    # Home configuration management
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "chaotic-nyx/nixpkgs";
+    };
+
+
+    #
+    # Development tooling
+    #
+
+    # Devshell to set up a development environment
+    devshell = {
+      url = "github:numtide/devshell";
+      flake = false;
+    };
+
+    # Easy linting of the flake and all kind of other stuff
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.flake-compat.follows = "chaotic-nyx/nixpkgs";
+      inputs.nixpkgs.follows = "chaotic-nyx/nixpkgs";
+    };
+
+    #
+    # Theming
+    #
+
+    # Beautiful pastel theming
+    catppuccin.url = "github:catppuccin/nix";
+    catppuccin-vsc = {
+      url = "github:catppuccin/vscode";
+      inputs.nixpkgs.follows = "chaotic-nyx/nixpkgs";
     };
   };
+  outputs =
+    { flake-parts
+    , nixpkgs
+    , pre-commit-hooks
+    , ...
+    } @ inputs:
+    let
+      internal = import ./internal {
+        inherit lib;
+        overlay = import ./packages/overlay.nix {
+          inherit inputs lib;
+        };
+        inputs = inputs // { inherit nixpkgs; };
+      };
+
+      lib = import ./lib { inherit inputs nixpkgs internal; };
+
+      perSystem =
+        { pkgs
+        , system
+        , ...
+        }:
+        let
+          packages = import ./packages { inherit system pkgs inputs lib; };
+        in
+        {
+          checks.pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            hooks = {
+              actionlint.enable = true;
+              commitizen.enable = true;
+              deadnix.enable = true;
+              nil.enable = true;
+              nixpkgs-fmt.enable = true;
+              prettier.enable = true;
+              statix.enable = true;
+              yamllint.enable = true;
+            };
+            src = ./.;
+          };
+
+          devShells = import ./devshell {
+            inherit inputs nixpkgs system pkgs packages;
+          };
+
+          formatter = pkgs.nixpkgs-fmt;
+
+          packages = packages.external;
+        };
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # Flake modules
+      imports = [ inputs.pre-commit-hooks.flakeModule ];
+
+      # The available systems
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      # Regular flake stuff
+      flake = {
+        inherit lib;
+        inherit internal;
+      };
+
+      # This applies to all systems
+      inherit perSystem;
+    };
 }
