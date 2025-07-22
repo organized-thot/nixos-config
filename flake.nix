31f7ef5ebb039b3e28b58eea5972f8cec85d1f08
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-07-21 19:43:01 -0500

    Delete Garuda dev files

--- flake.nix
+++ flake.nix
@@ -3,37 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     garuda.url = "github:garuda-linux/garuda-nix-subsystem/stable";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    chaotic.inputs.nixpkgs.follows = "nixpkgs";
     screenpipe-flake.url = "path:./screenpipe-flake";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, garuda, chaotic, ... }: {
+  outputs = { nixpkgs, garuda, ... }: {
     overlays = {
       default = import ./overlays/default.nix;
-      ffmpeg-overlay = import ./overlays/ffmpeg-overlay.nix;
+      # ffmpeg-overlay = import ./overlays/ffmpeg-overlay.nix;
     };
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hardware-configuration.nix
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        garuda.nixosModules.default
-        chaotic.nixosModules.default
-      ];
-      specialArgs = {
-        inherit inputs;
-        garuda = inputs.garuda;
-        chaotic = inputs.chaotic;
-        overlays = [
-          inputs.self.overlays.default
-          inputs.self.overlays.ffmpeg-overlay
+    nixosConfigurations.nixos = garuda.lib.garudaSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
         ];
       };
     };
-  };
-}
+  }
