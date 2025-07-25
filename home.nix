23e3b81da00c5f02755efd8c35f86f7b9ed87793
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-07-25 02:58:15 -0500

    Track home.nix

--- home.nix
+++ home.nix
@@ -18,6 +18,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+  	pkgs.podman
+  	pkgs.podman-compose
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
