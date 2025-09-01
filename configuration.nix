9fcd056e79679d331615a1e9a911c17c185383b9
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-09-01 15:40:04 -0500

    Remove console.font declaration to resolve nixos-rebuild conflic error

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,6 @@
   time.timeZone = "America/Chicago"; # Set your time zone.
   i18n.defaultLocale = "en_US.UTF-8"; # Select internationalisation properties.
   console = {
-    font = "Lat2-Terminus16";
     keyMap = "us";
     useXkbConfig = true; # use xkb.options in tty.
   };
