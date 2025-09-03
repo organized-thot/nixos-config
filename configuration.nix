a8274a0e8641aa15bde8dc725a444f91a86a4283
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-09-02 20:52:21 -0500

    Correct nix.optimise.automatic package declaration in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -69,8 +69,8 @@
 #       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
 #      ];
 #    package = pkgs.nix;
-    };
     optimise.automatic = true;
+    };
   };
 
   nixpkgs.config = {
