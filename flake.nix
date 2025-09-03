6a6abbb1ce86249745f6e0b2f3795474d97e5806
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-09-02 21:38:18 -0500

    Finish adding inputs.nixpkgs/flake-parts.follows declarations

--- flake.nix
+++ flake.nix
@@ -2,20 +2,25 @@
   description = "NixOS flake v0.3 for personal system with Garuda Nix Subsystem, Home Manager, and numerous other imputs.";
 
   inputs = {
-    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/0.1.397.tar.gz";
-    hardware.url = "https://flakehub.com/f/NixOS/nixos-hardware/0.1.2295.tar.gz";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.854036.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2505.tar.gz";
+
     garuda = {
       url = "gitlab:garuda-linux/garuda-nix-subsystem/stable";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nur = {
-      url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
     };
 
     fh.url = "https://flakehub.com/f/DeterminateSystems/fh/\*.tar.gz";
+
+    hardware.url = "https://flakehub.com/f/NixOS/nixos-hardware/0.1.2295.tar.gz";
+
+    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/0.1.397.tar.gz";
+
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
 
     nix-snapd = {
       url = "https://flakehub.com/f/nix-community/nix-snapd/0.1.62.tar.gz";
@@ -29,7 +34,11 @@
       inputs.flake-parts.follows = "flake-parts";
     };
 
-    TagStudio.url = "https://github.com/TagStudioDev/TagStudio/archive/refs/tags/v9.5.3.tar.gz";
+    TagStudio = { 
+      url = "https://github.com/TagStudioDev/TagStudio/archive/refs/tags/v9.5.3.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
 
   };
 
