d9ae00b455d460c39e3558a2587a9c61d41c9de9
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-10-07 07:36:08 -0500

    Remove Home manager from flake again (I think Aider reincorporated? Unsure about in-progress cherry-picking and branch confusion).

--- flake.nix
+++ flake.nix
@@ -1,33 +1,71 @@
 {
-  description = "Your new nix config";
+  description = "NixOS flake v0.3 for personal system with Garuda Nix Subsystem, and numerous other imputs.";
 
   inputs = {
-    # Nixpkgs
-    stylix.url = "https://flakehub.com/f/nix-community/stylix/0.1.1208.tar.gz";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
 
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
+    garuda = {
+      url = "gitlab:garuda-linux/garuda-nix-subsystem/stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager, stylix,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
-        modules = [./nixos/configuration.nix];
-      };
+    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/\*.tar.gz";
+
+    hardware.url = "https://flakehub.com/f/NixOS/nixos-hardware/0.1.2295.tar.gz";
+
+    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/0.1.397.tar.gz";
+
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+
+    nix-snapd = {
+      url = "https://flakehub.com/f/nix-community/nix-snapd/0.1.62+rev-355a1ed0141b6fd3093e3cb7b3492e6e67913681.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+
+    nixified-ai = {
+      url = "https://flakehub.com/f/nixified-ai/flake/0.1.76.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+
+    TagStudio = { 
+      url = "https://github.com/TagStudioDev/TagStudio/archive/refs/tags/v9.5.3.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
     };
 
   };
+
+  outputs = { 
+    self, 
+    hardware,
+    nixpkgs, 
+    garuda, 
+    nur, 
+    fh, 
+    nix-snapd, 
+    nixified-ai, 
+    TagStudio, flake-parts,
+    ... }: 
+  let
+    system = "x86_64-linux";
+  in
+  { 
+    nixosConfigurations.nixos = garuda.lib.garudaSystem {
+      inherit system;
+        modules = [
+          ./configuration.nix
+           nix-snapd.nixosModules.default { # snapd for NixOS            
+              services.snap.enable = true;
+           }
+        ];
+      };
+    };
 }
+
