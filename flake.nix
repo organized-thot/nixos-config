f9253d4d994628ed6394df7203ad11605c364669
Organized Thot <189394231+organized-thot@users.noreply.github.com>
2025-11-06 16:54:48 -0600

    Temporarily revert to basic default flake.nix

--- flake.nix
+++ flake.nix
@@ -1,64 +1,15 @@
 {
-  description = "NixOS flake v0.3 for personal system with Garuda Nix Subsystem, and numerous other imputs.";
+  description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/8913c168d1c56dc49a7718685968f38752171c3b";
-
-    garuda = {
-      url = "gitlab:garuda-linux/garuda-nix-subsystem/a20c1e2022dd197da22cb9b470840f5ffab105a2";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    fh.url = "github:DeterminateSystems/fh/3abaad486737b012e3ecef83e15b4c98b8293424";
-
-    hardware.url = "github:NixOS/nixos-hardware/3441b5242af7577230a78ffb03542add264179ab";
-
-    nur = {
-      url = "github:nix-community/NUR/3db8cd4bcccced0748d3896068ac2ead64b640a4";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-snapd = {
-      url = "github:nix-community/nix-snapd/355a1ed0141b6fd3093e3cb7b3492e6e67913681";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixified-ai = {
-      url = "github:nixified-ai/flake/7172cd645d145400feaad9983e05faff72e3113f";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    TagStudio = { 
-      url = "github:TagStudioDev/TagStudio/137c750595fb63a3c3297849f55e9bc32b4fe613";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { 
-    self, 
-    hardware,
-    nixpkgs, 
-    garuda, 
-    nur, 
-    fh, 
-    nix-snapd, 
-    nixified-ai, 
-    TagStudio,
-    ... }: 
-  let
-    system = "x86_64-linux";
-  in
-  { 
-    nixosConfigurations.nixos = garuda.lib.garudaSystem {
-      inherit system;
-        modules = [
-          ./nixos/configuration.nix
-           nix-snapd.nixosModules.default { # snapd for NixOS            
-              services.snap.enable = true;
-           }
-        ];
-      };
-    };
-}
+  outputs = { self, nixpkgs }: {
 
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
